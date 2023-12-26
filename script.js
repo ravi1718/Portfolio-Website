43734ca9915a1f88df184ab1f31868bf6f3cde02
ravi1718 <neeliravitej@gmail.com>
2023-12-26 19:22:03 +0530

    commit

---
+++ script.js
@@ -0,0 +1,24 @@
+function scrollToHome() {
+  var element = document.getElementById("first");
+  element.scrollIntoView({ behavior: "smooth" });
+}
+function scrollToAbout() {
+  var element = document.getElementById("secondsection");
+  element.scrollIntoView({ behavior: "smooth" });
+}
+function scrollToExperience() {
+  var element = document.getElementById("thirdsection");
+  element.scrollIntoView({ behavior: "smooth" });
+}
+function scrollToCertificates() {
+  var element = document.getElementById("Certificates");
+  element.scrollIntoView({ behavior: "smooth" });
+}
+function scrollToProjects() {
+  var element = document.getElementById("Projects");
+  element.scrollIntoView({ behavior: "smooth" });
+}
+function scrollToContact() {
+  var element = document.getElementById("Contact");
+  element.scrollIntoView({ behavior: "smooth" });
+}
